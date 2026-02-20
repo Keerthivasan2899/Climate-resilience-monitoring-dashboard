3d3a75cd1adb68423afeeb49be99d88f39270df1
Keerthivasan <vasank281999@gmail.com>
2026-02-20 08:20:09 +0530

    Refactor geometry handling and prepare regional NDVI processing

--- gee/continent_geometry.py
+++ gee/continent_geometry.py
@@ -1,15 +1,25 @@
-def get_americas():
+import ee
+
+
+def get_north():
     countries = ee.FeatureCollection("FAO/GAUL/2015/level0")
+    return countries.filter(ee.Filter.inList("ADM0_NAME", [
+        "United States of America", "Canada", "Mexico"
+    ]))
 
-    americas = countries.filter(
-        ee.Filter.inList("ADM0_NAME", [
-            "United States of America", "Canada", "Mexico",
-            "Guatemala", "Belize", "Honduras", "El Salvador",
-            "Nicaragua", "Costa Rica", "Panama",
-            "Brazil", "Argentina", "Chile", "Peru", "Colombia",
-            "Venezuela", "Bolivia", "Ecuador", "Paraguay",
-            "Uruguay", "Guyana", "Suriname"
-        ])
-    )
 
-    return americas.union().geometry()
+def get_central():
+    countries = ee.FeatureCollection("FAO/GAUL/2015/level0")
+    return countries.filter(ee.Filter.inList("ADM0_NAME", [
+        "Guatemala", "Belize", "Honduras", "El Salvador",
+        "Nicaragua", "Costa Rica", "Panama"
+    ]))
+
+
+def get_south():
+    countries = ee.FeatureCollection("FAO/GAUL/2015/level0")
+    return countries.filter(ee.Filter.inList("ADM0_NAME", [
+        "Brazil", "Argentina", "Chile", "Peru", "Colombia",
+        "Venezuela", "Bolivia", "Ecuador", "Paraguay",
+        "Uruguay", "Guyana", "Suriname"
+    ]))
---
+++ gee/ndvi_north.py
@@ -0,0 +1,52 @@
+import ee
+from continent_geometry import get_north
+
+ee.Initialize(project="gee-climate-dashboard-487203")
+
+region = get_north()
+
+collection = (
+    ee.ImageCollection("MODIS/061/MOD13Q1")
+    .select("NDVI")
+    .filterDate("2021-01-01", "2025-12-31")
+    .filterBounds(region)
+)
+
+
+def monthly_ndvi(year, month):
+    start = ee.Date.fromYMD(year, month, 1)
+    end = start.advance(1, "month")
+
+    img = collection.filterDate(start, end).mean()
+
+    stats = img.reduceRegion(
+        reducer=ee.Reducer.mean(),
+        geometry=region.geometry(),
+        scale=5000,
+        maxPixels=1e13
+    )
+
+    return ee.Feature(None, {
+        "year": year,
+        "month": month,
+        "ndvi": stats.get("NDVI")
+    })
+
+
+years = ee.List.sequence(2021, 2025)
+months = ee.List.sequence(1, 12)
+
+features = years.map(lambda y: months.map(
+    lambda m: monthly_ndvi(y, m))).flatten()
+fc = ee.FeatureCollection(features)
+
+task = ee.batch.Export.table.toDrive(
+    collection=fc,
+    description="NDVI_North_America",
+    folder="GEE_Exports",
+    fileNamePrefix="ndvi_north_2021_2025",
+    fileFormat="CSV"
+)
+
+task.start()
+print("North America NDVI export started")
